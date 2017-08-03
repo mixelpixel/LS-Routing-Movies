85e3b258be33f65c6355c7be3262b90cf3d3f802
mixelpixel <mixelpix@mac.com>
2017-08-03 19:17:44 -0400

    basic react redux router set up with redux devtools

--- patrick/movies/src/index.js
+++ patrick/movies/src/index.js
@@ -1,13 +1,30 @@
+// REACT
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/index.css';
-import App from './components/App';
+// REDUX
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
+// REACT-ROUTER
 import { BrowserRouter as Router } from 'react-router-dom';
+// <body>...</body>
+import './css/index.css';
+// App
+import App from './components/App';
+// REDUCERS
 import rootReducer from './reducers';
 
 
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+
+const STORAGE_WAREHOUSE = createStoreWithMiddleware(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store = { STORAGE_WAREHOUSE }>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root'));
