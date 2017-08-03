604ba2c134b49d2660b00d425a123ec307fd0bae
mixelpixel <mixelpix@mac.com>
2017-08-03 15:09:52 -0400

    idem

--- patrick/movies/src/components/App.js
+++ patrick/movies/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from '../art/logo.svg';
+import routerMagic from '../art/routing.png';
 import '../css/App.css';
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to Lambda School: React-Routing</h2>
+          <img src={routerMagic} alt='Friends'></img>
         </div>
         <p className="App-intro">
           Something <code>AMAZING</code> will go here!
