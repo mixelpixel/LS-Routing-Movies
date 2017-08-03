809c2e65b4be4b56fcf93dfd7f40fd2488a3edaa
mixelpixel <mixelpix@mac.com>
2017-08-03 18:59:02 -0400

    set up

--- patrick/movies/src/index.js
+++ patrick/movies/src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './components/App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import rootReducer from './reducers';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById('root'));
