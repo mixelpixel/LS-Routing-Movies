32e619f3c74f84740b797d194cbb6313a21cd234
mixelpixel <mixelpix@mac.com>
2017-08-04 11:56:33 -0400

    for later?

--- patrick/movies/src/reducers/GetMoviesReducer.js
+++ patrick/movies/src/reducers/GetMoviesReducer.js
@@ -10,7 +10,7 @@ import { GET_MOVIES } from '../actions';
 const moviesReducer = (movies = [], action) => {
   switch(action.type) {
     case GET_MOVIES:
-      if (action.error) return movies;
+      // if (action.error) return movies;
       return action.payload.data;
     default:
       return movies;
