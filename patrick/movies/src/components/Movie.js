291c14b3a5a92a4405e45c434142c4f489117329
mixelpixel <mixelpix@mac.com>
2017-08-04 17:11:21 -0400

    use id# for route instead of movie

--- patrick/movies/src/components/Movie.js
+++ patrick/movies/src/components/Movie.js
@@ -5,7 +5,8 @@ import { Link } from 'react-router-dom';
 const Movie = ({ movie, i }) => {
   return (
     <li key={ i }>
-      <Link to={ `/${movie.title}` }>{ `Title: ${movie.title}` }</Link>
+      <Link to={ `/${movie.id}` }>{ `Title: ${movie.title}` }</Link>
+      {/* <Link to={ `/${movie.title}` }>{ `Title: ${movie.title}` }</Link> */}
       {/* <p>{ `Title: ${movie.title}` }</p> {/* Link tag goes here to invoke click */}
     </li>
   );
