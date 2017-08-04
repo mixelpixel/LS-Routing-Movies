0623498bd6997c91f695b99ebe4c1c409dcad8f6
mixelpixel <mixelpix@mac.com>
2017-08-04 17:31:05 -0400

    SINGULAR instead of plural

--- patrick/movies/src/reducers/GetMovieByIDReducer.js
+++ patrick/movies/src/reducers/GetMovieByIDReducer.js
@@ -1,12 +1,12 @@
 import { GET_MOVIE_BY_ID } from '../actions';
 
-const movieByIDReducer = (movies = {}, action) => {
+const movieByIDReducer = (movie = {}, action) => {
   switch(action.type) {
     case GET_MOVIE_BY_ID:
       // if (action.error) return movies; //<~~~ this/catch magic?
       return action.payload.data;
     default:
-      return movies;
+      return movie;
   }
 };
 
